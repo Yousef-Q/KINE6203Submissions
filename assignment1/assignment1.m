c244ac3781ee4bea2c197b5b79b65907a312c799
Yousef-Q <qadumiy24@students.ecu.edu>
2024-08-28 10:28:43 -0400

    Assignment 1 Submission

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -50,6 +50,10 @@ averageAnswer = mean(averageAnswer)
 
 help images
 doc images 
+%ChatGPT was used to help answer this question. I typed "How to list
+%availible functions in the image processing toolbox." help images displays
+%the available functions in the images toolbox while doc images outputs the
+%image processing toolbox documentation 
 
 
 % 6. On the line below figure, write a command to draw a graph of 
@@ -109,6 +113,8 @@ S = sum(A,"omitnan")
 % the game if so. 
 
 xpbombs
+%ChatGPT was used to answer this question. I typed "How to play minesweeper
+%on Matlab?". xpbombs is the built in matlab function that output the game.
 
 
 % 12. Create a variable myAge and assign it the value of your age. Then 
@@ -147,7 +153,7 @@ whos
 % variable to the type int32 and store the result in a new variable (hint: this is known
 % as casting in computer programming). Use whos to check your result. 
 
-appropriatelyNamedVariable: 32.5
+appropriatelyNamedVariable = 32.5
 NewVariable = int32(appropriatelyNamedVariable)
 
 % 16. Create a variable called weightInLBs to store a weight in pounds. Convert this to 
@@ -179,14 +185,16 @@ vecC = 8:-2:2
 % 20. Give a MATLAB expression that would create a vector 
 % (assigned to a variable called vec) of 50 elements that range, equally spaced, 
 % from 0 to 2pi. 
-
+j = (2*pi)/50
+vec = 0:j:2*pi
 
 
 
 % 21. Using the colon operator and the transpose operator, 
 % create a column vector that has the values -1 to 1 in steps of 0.5.
 % Assign your answer to a variable called colVec.
-
+columnVector = -1:0.5:1
+colVec = columnVector.'
 
 
 
@@ -196,6 +204,9 @@ vecC = 8:-2:2
 % dimensions given by the values of rows and cols. The resultMat should
 % change each time you execute your code. 
 
+rows = randi([1,5])
+cols = randi([1,5])
+resultMat = zeros(rows,cols)
 
 
 
@@ -204,18 +215,19 @@ vecC = 8:-2:2
 % the following on the original vector and store your results in appropriately 
 % named variables. (you should have a seperate line of code for each)
 
+originalvec = randi([-10,10],5,1)
 % - subtract 3 from each element
-
+subtractVec = originalvec - 3
 
 % - count how many are positive
-
+countVec = length(nonzeros(originalvec(originalvec>0)))
 
 % - get the absolute value of each element
-
+absoluteVec = abs(originalvec)
 
 
 % - find the maximum.
-
+maxVec = max(originalvec)
 
 
 % 24. Write some code that will calculate the area of a trapezoid. Create
@@ -223,6 +235,13 @@ vecC = 8:-2:2
 % result of your calculation in a variable called trapArea. Comment your
 % code so that another user can understand what your code does. 
 
+%Define variables of the bases of a trapezoid
+base1 = 10  
+base2 = 20 
+height = 15
+
+%Calculates the area of the trapezoid
+trapArea = 1/2*(base1+base2)*height
 
 
 
@@ -237,8 +256,10 @@ vecC = 8:-2:2
 % costs and then calculates the ROI and stores the value in a variable called
 % ROI.
 
+totalRevenues = input('Enter total revenues here:')
+totalCosts = input('Enter total costs here:')
 
-
+ROI = (totalRevenues - totalCosts) / totalCosts * 100
 
 
 %% 
@@ -251,9 +272,17 @@ vecC = 8:-2:2
 % part 2, make sure you click the Run Section button vs the Run button) 
 
 %%% Enter the code for your program below this line
+userInput = input('Please enter text here: ', "s")
+msgbox(userInput,'Your Text')
 
-
-
+%ChatGPT was used to help complete this questions. At first I thought I had
+%to make a grpah to display the text so I asked ChatGPT if there was a
+%specific function that would allow me to display the tex10t on the screen
+%rather than the command window. I then asked it if the user can
+%personalize the message themselves. Teh 'msgbox' function displays the
+%text on the user input in matlab. The input fucntion prompts the user to
+%type a message into the command window, and the 's' fucntion specifies the
+%input is a string. 
 
 
 
